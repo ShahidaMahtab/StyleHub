a24dcd53da89d377f88358ce1c33be5d5e501f24
Shahida Mahtab <marwa.mahtab@gmail.com>
2023-06-01 12:12:18 +0600

    about nav

--- app/page.js
+++ app/page.js
@@ -1,9 +1,41 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Home() {
-  return (
-    <main>
-      <h1>main</h1>
-    </main>
-  );
+	return (
+		<section className=''>
+			<div className='relative'>
+				<Image
+					src='https://source.unsplash.com/7cERndkOyDw'
+					alt='Banner Image'
+					width={1690}
+					height={400}
+					className='center bg-no-repeat brightness-[40%] cover '
+				/>
+				<div className='absolute inset-0 flex top-28 justify-start container mx-auto'>
+					<div className='space-y-4'>
+						<div className='font-semibold text-4xl text-white uppercase'>
+							Fosfo puffer
+						</div>
+						<br />
+						<div className='text-8xl text-white font-bold'>
+							Eco-Friendly &
+						</div>
+						<br />
+						<div className='font-semibold text-4xl text-white '>
+							on trend.
+						</div>
+						{/* button */}
+						<div className='flex space-x-4 text-black font-semibold pt-4'>
+							<button className='bg-white rounded-xl py-2 px-3'>
+								Shop Men
+							</button>
+							<button className='bg-white rounded-xl py-2 px-3'>
+								Shop Women
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 }
