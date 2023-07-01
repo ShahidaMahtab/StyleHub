0db2a9e0701b09c6f0ccbf9e7e0c41576fc1d243
SanjidaNowrin <nowrin.cse18@gmail.com>
2023-07-01 13:53:01 +0600

    pagination-added

--- app/api/(styleguide)/[styleguide]/route.js
+++ app/api/(styleguide)/[styleguide]/route.js
@@ -7,10 +7,28 @@ export async function GET(request, { params }) {
   const { styleguide } = params;
   try {
     await connect();
-    const styleCategory = await Styleguide.find({ type: styleguide });
+    // Extract query parameters
+    const searchParams = new URL(request.url).searchParams;
+    const page = searchParams.get("page") || 1;
+
+    const perPage = 10;
+
+    // Calculate skip value for pagination
+    const skip = (page - 1) * perPage;
+    const styleCategory = await Styleguide.find(
+      { type: styleguide }.skip(skip).limit(perPage)
+    );
+    // Get total number of products
+    const totalProducts = await Styleguide.countDocuments({});
+
+    // Calculate total number of pages
+    const totalPages = Math.ceil(totalProducts / perPage);
     return NextResponse.json({
       product: styleCategory,
       count: styleCategory.length,
+      currentPage: parseInt(page),
+      totalPages,
+      perPage,
     });
   } catch (err) {
     return new NextResponse(`No product with category : ${styleguide}`);
--- app/api/products/route.js
+++ app/api/products/route.js
@@ -2,12 +2,38 @@ import connect from "@/middleware/mongoose";
 import Products from "@/models/Products";
 import { NextResponse } from "next/server";
 
+// get all product
 export async function GET(request) {
   try {
     await connect();
-    const products = await Products.find({});
-    return NextResponse.json({ products });
+    // console.log(request.url);
+
+    // Extract query parameters
+    const searchParams = new URL(request.url).searchParams;
+    const page = searchParams.get("page") || 1;
+
+    const perPage = 10;
+
+    // Calculate skip value for pagination
+    const skip = (page - 1) * perPage;
+
+    // Fetch products with pagination
+    const products = await Products.find({}).skip(skip).limit(perPage);
+
+    // Get total number of products
+    const totalProducts = await Products.countDocuments({});
+
+    // Calculate total number of pages
+    const totalPages = Math.ceil(totalProducts / perPage);
+
+    return NextResponse.json({
+      products,
+      currentPage: parseInt(page),
+      totalPages,
+      perPage,
+    });
   } catch (err) {
+    console.log(err);
     return new NextResponse(JSON.stringify({ error: "Database Error" }), {
       status: 500,
     });
