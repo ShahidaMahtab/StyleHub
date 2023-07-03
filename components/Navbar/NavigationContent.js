ce9064fd3bc54fd286d42cbe7b5eb0f60d0a014e
shahidamahtab <marwa.mahtab@gmail.com>
2023-07-03 09:06:14 +0600

    bug fixed

--- components/Navbar/NavigationContent.js
+++ components/Navbar/NavigationContent.js
@@ -1,5 +1,5 @@
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
 import { Collapse } from 'antd';
 import Link from 'next/link';
 import Fade from 'react-reveal/Fade';
